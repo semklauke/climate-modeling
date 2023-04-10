eedd57453203d8cf961001de37383fc5a965468c
semklauke <privat@semklauke.de>
2023-04-10 23:57:27 +0200

    mile1: sattled on v3

--- milestone1/contours_test.jl
+++ milestone1/contours_test.jl
@@ -17,23 +17,92 @@ function interpolateBetweenPoints(X::Matrix, Y::Matrix, x, y)
     end
 end
 
-# Contour.convert(::Type{Curve2{Float64}}, x::Curve2{Int64}) = Curve2(SVector{2,Float64}[
-#     SVector{2,Float64}(convert(Float64, t[1]),convert(Float64, t[2])) for t in x.vertices
-# ])
+###### Version 1 ######
+# using Plots
+function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
+    plot(legend=false, size=(1200,610), axis = nothing, ticks= false, title="Earth Plot")
+    for lat=1:64, long=1:127
+        rect = Shape(
+            [ X[lat, long], X[lat, long+1], X[lat, long+1], X[lat, long] ],
+            [ Y[lat, long],  Y[lat, long], Y[lat+1, long], Y[lat+1, long]])
 
-for cl in levels(contours(1.0:1.0:65.0,1.0:1.0:128.0,T,[1,2,3,5]))
-        lvl = level(cl)
-        for line in lines(cl)
+        plot!(rect, fill=color_lookup[T[lat, long]], linewidth=0.2, linecolor=color_lookup[T[lat, long]])
+    end
+    savefig("projection.pdf")
+end
+
+####### Version 2 ######
+# using Plots
+function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
+    plot(legend=false, size=(1200,610))
+    for surface_type in [5,2,3,1]
+        # add zeros around to detect the edge
+        T_contour = zeros(Int, 67, 130)
+        # filter in only the current surface_type
+        T_contour[2:66, 2:129] = (i -> Int(i == surface_type)).(T)
+        # find contour in T_contour
+        lvls = levels(contours(Float64.(1:67),Float64.(1:130),T_contour,1))
+        # loop throug isolines
+        for line in lines(lvls[1])
             xs, ys = coordinates(line)
             Xs = []
             Ys = []
+            color = color_lookup[surface_type]
             for (x,y) in zip(xs,ys)
+                x -= 1.0
+                x = max(1.0, x)
+                y -= 1.0
+                y = max(1.0, y)
                 x_inter, y_inter = interpolateBetweenPoints(X,Y,x,y)
                 push!(Xs, x_inter)
                 push!(Ys, y_inter)
-                # push!(Xs, X[round(Int, x), round(Int, y)])
-                # push!(Ys, Y[round(Int, x), round(Int, y)])
             end
-            plot!(Xs, Ys, color=:white, linewidth=0.5)
+            plot!(Shape(Xs, Ys), fill=color_lookup[surface_type], linewidth=0.1)
         end
     end
+    savefig("projection.pdf")
+end
+
+###### Version 3 ######
+# using PyPlots
+function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
+    fig, ax0 = subplots(figsize=(13,6), dpi=110) 
+    earth = contourf(X,Y,T, levels=[0,1,2,3,5,6] ,colors=[(0.16, 0.38, 0.09),(0.71, 0.77, 0.86),(0.90, 0.90, 0.97),(0.00, 0.03, 0.48), "none"])
+    fig.colorbar(earth, ax=ax0, ticks=[1,2,3,5])
+    ax0.set_axis_off()
+    ax0.set_title("Earth Plot")
+    savefig("projection.pdf")
+end
+
+###### Version 4 ######
+# using PyPlots
+function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
+    fig, ax0 = subplots(figsize=(13,6), dpi=110) 
+    for surface_type in [5,3,2,1]
+        T_contour = (i -> Int(i == surface_type)).(T)
+        earth = contourf(X,Y,T_contour, levels=1 ,colors=[(0,0,0,0), color_lookup_py[surface_type]])
+    end
+    ax0.set_title("Earth Plot")
+    ax0.set_axis_off()
+    savefig("projection.pdf")
+end
+
+###### Version 5 ######
+# using PyPlots
+function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
+    fig, ax0 = subplots(figsize=(13,6), dpi=110) 
+    colormap = ColorMap("test", [0.00 0.90 0.71 0.16; 
+                                 0.03 0.90 0.77 0.38;
+                                 0.48 0.97 0.86 0.09], 4)
+    earth = ax0.pcolormesh(X, Y, T, edgecolors="none", cmap="viridis_r")
+    #fig.colorbar(earth, ax=ax0, ticks=[1,2,3,5])
+    ax0.set_axis_off()
+    ax0.set_title("Earth Plot")
+    savefig("projection.pdf")
+end
+
+####### Stuff ######
+
+Contour.convert(::Type{Curve2{Float64}}, x::Curve2{Int64}) = Curve2(SVector{2,Float64}[
+    SVector{2,Float64}(convert(Float64, t[1]),convert(Float64, t[2])) for t in x.vertices
+])
--- milestone1/main.jl
+++ milestone1/main.jl
@@ -1,12 +1,16 @@
-using Plots
-using Images
+
+#using Plots
+#using Images
+using PyPlot
+
+# Installing PyPlot: https://github.com/JuliaPy/PyPlot.jl#installation
 
 function read_geography()
     file = open("The_World128x65.dat")
     lines = readlines(file)
     close(file)
     vectors = map(l -> map(s -> parse(Int8, s), split(l, " ")), lines)
-    T = mapreduce(permutedims, vcat, vectors)
+    T = mapreduce(permutedims, vcat, reverse(vectors))
     println("read input matrix T of dimensions $(size(T))")
     return T
 end
@@ -19,32 +23,30 @@ function robinson_projection(T::Matrix{Int8})
         𝜑 = deg2rad(-180.0 + (long-1)* step_size)
         𝜃 = deg2rad(-90.0 + (lat-1)* step_size)
         X[lat, long] = (𝜑/pi) * (0.0379*(𝜃^6) - 0.15*(𝜃^4) - 0.367*(𝜃^2) + 2.666)
-        Y[lat, long] = (0.96047*𝜃 - 0.00857*sign(𝜃)*(abs(𝜃)^(6.41)))*(-1.0)
+        Y[lat, long] = (0.96047*𝜃 - 0.00857*sign(𝜃)*(abs(𝜃)^(6.41)))
     end
     return X, Y
+end
+
+function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
+    fig, ax0 = subplots(figsize=(13,6), dpi=110) 
+    earth = contourf(X,Y,T, 
+                levels=[0,1,2,3,5,6],
+                colors=[(0.16, 0.38, 0.09),(0.71, 0.77, 0.86),(0.90, 0.90, 0.97),(0.00, 0.03, 0.48), "none"]
+            )
+    fig.colorbar(earth, ax=ax0, ticks=[1,2,3,5])
+    ax0.set_axis_off()
+    ax0.set_title("Earth Plot")
+    savefig("projection.pdf")
 end
 
 color_lookup = Dict(
     5 => RGB(0.00, 0.03, 0.48),
     3 => RGB(0.90, 0.90, 0.97),
     2 => RGB(0.71, 0.77, 0.86),
-    1 => RGB(0.16, 0.38, 0.09)
+    1 => RGB(0.16, 0.38, 0.09),
 )
-
-function plot_geo(X::Matrix, Y::Matrix, T::Matrix{Int8})
-    plot(legend=false, size=(1200,610))
-    for lat=1:64, long=1:127
-        rect = Shape(
-            [ X[lat, long], X[lat, long+1], X[lat, long+1], X[lat, long] ],
-            [ Y[lat, long],  Y[lat, long], Y[lat+1, long], Y[lat+1, long]])
-
-        plot!(rect, fill=color_lookup[T[lat, long]], linealpha=1, linewidth=1, linecolor=color_lookup[T[lat, long]])
-    end
-    savefig("projection.pdf")
-end
-
-function plot_without_projection()
-    T = read_geography()
+function plot_without_projection(T::Matrix{Int8})
     G = map(p -> color_lookup[p], T)
     img = colorview(RGB, G)
     plot(img)
@@ -55,4 +57,4 @@ T = read_geography()
 G = robinson_projection(T)
 plot_geo(G[1], G[2], T)
 
-#plot_without_projection()
+#plot_without_projection(T)
